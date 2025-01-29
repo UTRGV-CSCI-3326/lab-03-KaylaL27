b54f939f6a8d0bb9ac1f3dbbb67468ffdeb6fe5b
Kayla <144965767+KaylaL27@users.noreply.github.com>
2025-01-29 15:46:23 +0000

    FInished lab

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,16 @@
+public class BankAccount{
+    public static void main(String[] args){
+        float balance = 2175.37f;
 
+        balance -= 302.5; //frist withdraw
+        balance += 50.03; //first deposit
+        balance /= 2; // second withdraw
+        balance += 20; //second deposit
+        balance -= 1; // third withdraw
+        balance *= 2; //third deposit
+        balance += 1; //fourth deposit
+         
+        //use string format to print balance with 2 decimal places
+        System.out.println("Final balance: " + String.format("%.2f", balance));
+    }
+}
